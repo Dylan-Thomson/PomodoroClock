5157880b7d03c0524e945e569065d0e7afd629dd
Dylan Thomson <djthomson88@gmail.com>
2017-08-05 13:59:05 -0400

    Set up listeners for -/+ buttons
    -User can set break length and session length using -/+ buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,31 @@
-alert("connected")
+// Document Ready
+$(function() {
+	initListeners();
+});
+
+var breakTime = Number($("#break").text());
+var sessionTime = Number($("#session").text());
+
+function initListeners() {
+	$("#breakLength .fa-minus").on("click", function() {
+		if(breakTime > 1) {
+			breakTime -= 1;
+			$("#break").text(breakTime);
+		}
+	});	
+	$("#breakLength .fa-plus").on("click", function() {
+		breakTime += 1;
+		$("#break").text(breakTime);
+
+	});	
+	$("#sessionLength .fa-minus").on("click", function() {
+		if(sessionTime > 1) {
+			sessionTime -= 1;
+			$("#session").text(sessionTime);
+		}
+	});	
+	$("#sessionLength .fa-plus").on("click", function() {
+		sessionTime += 1;
+		$("#session").text(sessionTime);
+	});
+}
